ef7ff02b6cfe30b2552be33e5127678f7e871d20
Mar-Prz <mark.damian.p@gmail.com>
2023-02-15 19:15:51 +1100

    week 4

--- server.js
+++ server.js
@@ -8,26 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
-//  const cardList = [
-//      {
-//          title: "Toyota",
-//          image: "images/car2.jpg",
-//          link: "About Toyota",
-//          description: "Demo description about Toyota",
-//      },
-//      {
-//          title: "Mustang",
-//          image: "images/car3.jpg",
-//          link: "About Mustang",
-//          description: "Demo description about Mustang",
-//      }
-//  ]
-
-//mongoDb connection
-const MongoClient = require('mongoDB').MongoClient; 
-const uri = 'mongodb+srv://MarPrz:Themailman3211@cluster0.xlpulu1.mongodb.net/?retryWrites=true&w=majority'
+//mongoDb connection...
+const MongoClient = require('mongodb').MongoClient; 
+const uri = 'mongodb+srv://abatool:amna@cluster0.xnvxmni.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(uri, {useNewUrlParser: true})
-
 
 const createCollection = (collectionName) => {
     client.connect((err,db) => {
@@ -42,12 +26,12 @@ const createCollection = (collectionName) => {
     })
 }
 
-//insert project
+//insert project....
 const insertProjects = (project,callback) => {
     projectCollection.insert(project,callback);
 }
 
-//post api
+//post api....
 app.post('/api/projects',(req,res) => {
     console.log("New Project added", req.body)
     var newProject = req.body;
@@ -61,9 +45,24 @@ app.post('/api/projects',(req,res) => {
     })
 })
 
+// const cardList = [
+//     {
+//         title: "Kitten 2",
+//         image: "images/kitten2.jpg",
+//         link: "About Kitten 2",
+//         desciption: "Demo desciption about kitten 2"
+//     },
+//     {
+//         title: "Kitten 3",
+//         image: "images/kitten2.jpg",
+//         link: "About Kitten 3",
+//         desciption: "Demo desciption about kitten 3"
+//     }
+// ]
 
+// get project...
 const getProjects = (callback) => {
-projectCollection.find({}).toArray(callback);
+    projectCollection.find({}).toArray(callback);
 }
 
 app.get('/api/projects',(req,res) => {
@@ -78,8 +77,7 @@ app.get('/api/projects',(req,res) => {
 })
 
 var port = process.env.port || 3000;
-app.listen(port,()=>
-{
-console.log("App listening to http://localhost:"+port)
-createCollection('Cars')
+app.listen(port,()=>{
+    console.log("App listening to http://localhost:"+port)
+    createCollection('Pets')
 })
